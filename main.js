97d96276e99db3fa2935c917d4f71b0b2bb9baee
Max Lawton <max@lawtonnation.com>
2023-07-09 10:26:54 -0400

    new comments

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // define buttons and divs
   var playButton = document.getElementById("playButton");
   var stopButton = document.getElementById("stopButton");
   var startMenu = document.getElementById("startMenu");
   var playMenu = document.getElementById("playMenu");
   var clock = document.getElementById("clock");
 
-  var timerInterval; // Holds the reference to the setInterval timer
+  var timerInterval; // holds the reference to the setInterval timer
 
+  // on play button press
   playButton.addEventListener("click", function() {
     startMenu.classList.toggle("hidden");
     playMenu.classList.toggle("hidden");
 
-    // Start the clock timer
+    // start the clock timer
     var startTime = Date.now(); // Record the start time
     timerInterval = setInterval(function() {
       var currentTime = Date.now();
@@ -20,12 +22,14 @@ document.addEventListener("DOMContentLoaded", function() {
       clock.textContent = formattedTime;
     }, 10);
 
+    // run the main game after clearing
     var container = document.getElementById("textContainer");
     container.innerHTML = ""
     run()
 
   });
 
+  // on stop button press
   stopButton.addEventListener("click", function() {
     startMenu.classList.toggle("hidden");
     playMenu.classList.toggle("hidden");
@@ -34,11 +38,12 @@ document.addEventListener("DOMContentLoaded", function() {
     var container = document.getElementById("textContainer");
     container.innerHTML = "Quitted. Press start to try again..."
 
-    // Stop the clock timer
+    // stop the clock timer
     clearInterval(timerInterval);
     clock.textContent = "0:00:000";
   });
 
+  // for time printing
   function formatTime(milliseconds) {
     var totalSeconds = Math.floor(milliseconds / 1000);
     var minutes = Math.floor(totalSeconds / 60);
@@ -47,6 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
     return minutes + ":" + padZero(seconds, 2) + ":" + padZero(milliseconds, 3);
   }
 
+  // time printing cont.
   function padZero(number, width) {
     var paddedNumber = number.toString();
     while (paddedNumber.length < width) {
@@ -54,4 +60,5 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     return paddedNumber;
   }
+
 });
