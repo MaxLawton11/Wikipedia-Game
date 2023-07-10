c2508eff35e9e5152fc42da4fd39ce21ea5ca877
Max Lawton <max@lawtonnation.com>
2023-07-09 21:32:33 -0400

    new clock setup

--- game.js
+++ game.js
@@ -80,6 +80,43 @@ function randomStart(words) {
 	return remainingWords
 }
 
+// ./clock.js
+
+function startClock(clockElement) {
+  var clock = document.getElementById("clock");
+  // start the clock timer
+  var startTime = Date.now(); // Record the start time
+  timerInterval = setInterval(function() {
+    var currentTime = Date.now();
+    var elapsedTime = currentTime - startTime;
+    var formattedTime = formatTime(elapsedTime);
+    clock.textContent = formattedTime;
+  }, 10);
+}
+
+function stopClock() {
+  clearInterval(timerInterval);
+  clock.textContent = "0:00:000";
+}
+
+// for time printing
+function formatTime(milliseconds) {
+  var totalSeconds = Math.floor(milliseconds / 1000);
+  var minutes = Math.floor(totalSeconds / 60);
+  var seconds = totalSeconds % 60;
+  var milliseconds = milliseconds % 1000;
+  return minutes + ":" + padZero(seconds, 2) + ":" + padZero(milliseconds, 3);
+}
+
+// time printing cont.
+function padZero(number, width) {
+  var paddedNumber = number.toString();
+  while (paddedNumber.length < width) {
+    paddedNumber = "0" + paddedNumber;
+  }
+  return paddedNumber;
+}
+
 // ./game.js
 
 function run() {
@@ -96,6 +133,7 @@ function run() {
       // Do something with the content
       var processedContent = textProcessor(content);
       console.log(processedContent);
+      startClock()
       displayWords(processedContent, container);
     })
     .catch(function(error) {
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function() {
   var stopButton = document.getElementById("stopButton");
   var startMenu = document.getElementById("startMenu");
   var playMenu = document.getElementById("playMenu");
-  var clock = document.getElementById("clock");
 
   var timerInterval; // holds the reference to the setInterval timer
 
@@ -12,15 +11,6 @@ document.addEventListener("DOMContentLoaded", function() {
   playButton.addEventListener("click", function() {
     startMenu.classList.toggle("hidden");
     playMenu.classList.toggle("hidden");
-
-    // start the clock timer
-    var startTime = Date.now(); // Record the start time
-    timerInterval = setInterval(function() {
-      var currentTime = Date.now();
-      var elapsedTime = currentTime - startTime;
-      var formattedTime = formatTime(elapsedTime);
-      clock.textContent = formattedTime;
-    }, 10);
 
     // run the main game after clearing
     var container = document.getElementById("textContainer");
@@ -37,28 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
     stopAnimation()
     var container = document.getElementById("textContainer");
     container.innerHTML = "Quitted. Press start to try again..."
+    stopClock()
 
-    // stop the clock timer
-    clearInterval(timerInterval);
-    clock.textContent = "0:00:000";
   });
-
-  // for time printing
-  function formatTime(milliseconds) {
-    var totalSeconds = Math.floor(milliseconds / 1000);
-    var minutes = Math.floor(totalSeconds / 60);
-    var seconds = totalSeconds % 60;
-    var milliseconds = milliseconds % 1000;
-    return minutes + ":" + padZero(seconds, 2) + ":" + padZero(milliseconds, 3);
-  }
-
-  // time printing cont.
-  function padZero(number, width) {
-    var paddedNumber = number.toString();
-    while (paddedNumber.length < width) {
-      paddedNumber = "0" + paddedNumber;
-    }
-    return paddedNumber;
-  }
-
 });
